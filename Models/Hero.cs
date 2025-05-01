6a804692b838e8afcf26f5e043b67836986de4ba
Mihail <swankyduke@gmail.com>
2025-05-01 23:47:30 +0200

    Add Hero class and few data for heroes with images

---
+++ Models/Hero.cs
@@ -0,0 +1,25 @@
+﻿namespace TorchBuild.Models
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public List<SubClass> SubClasses { get; set; }
+      
+    }
+
+    public class SubClass
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public int HeroSkillSlots { get; set; }
+        public List<SkillSlot> UniqueSkills { get; set; }
+
+    }
+
+    public class SkillSlot
+    {
+        public int Slot { get; set; }
+        public List<string> HeroSkillChoices { get; set; }
+        public int PassiveSkillSlotCount { get; set; }
+    }
+}
