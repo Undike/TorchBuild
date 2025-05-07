6c98afefac4129b18a02de61feab153860a58065
Mihail <swankyduke@gmail.com>
2025-05-07 14:53:38 +0200

    Added Second God talent branches

--- Models/Stats/AttributeModifierType .cs
+++ Models/Stats/AttributeModifierType .cs
@@ -2,53 +2,263 @@
 {
     public enum AttributeModifierType
     {
+        //Icrease DAMAGE
         IncreaseDamage,
         IncreaseAttackDamage,
+        IncreaseAttackDamageWhenHoldingTwoHandedWeapon,
         IncreasePhysicalDamage,
-        IncreaseSpellDamage,
         IncreaseElementalDamage,
+
+        // Spell
+        IncreaseSpellDamage,
+        IncreaseCastSpeedPerStackOfAgilityBlessing,
+        IncreaseCastSpeedPerDexterity,
+        IncreaseChanceToTriggerMainSpellSkillAdditionalTimeWhenUsingIt,
+
+        //Fire
         IncreaseFireDamage,
-        IncreaseColdDamage,
-        IncreaseLightningDamage,
-        IncreaseErosionDamage,
-        IncreaseCriticalStrikeDamage,
-        IncreaseCriticalChance,
-        IncreaseAttackCriticalStrikeRating,
-        IncreaseRangedAttackCriticalStrikeRating,
-        IncreaseCriticalStrikeDamageToMeleeAttacks,
-        IncreaseAttackSpeed,
-        IncreaseCastSpeed,
-
-        IncreaseFireSkillArea,
-        IncreaseSkillArea,
-
+        IncreaseFirePenetration,
+        IncreaseMinionFirePenetration,
+        IncreaseMinionFireDamage,
         IncreaseIgniteDamage,
         IncreaseIgnateChance,
-        IncreaseProjectileSpeed,
+        IncreaseIgnatechanceForMinions,
+        IncreaseFireDamageAgainstLowLifeEnemies,
+        IncreaseMinionFireDamageAgainstLowLifeEnemies,
+        IncreaseFireDamageOverTime,
+        AddFlatBaseIgniteDamage,
+        InflictAdditionalStackOfIgnite,
+        IncreaseFireDamagePerStrength,
+        //Convert To Fire Damage
+        ConvertPhysicalDamageToFireDamage,
+        ConvertMinionPhysicalDamageToFireDamage,
 
+        //Cold
+        IncreaseColdDamage,
+
+        //Lightning
+        IncreaseLightningDamage,
+        IncreaseMinionLightningDamage,
+        IncreaseNumbedChance,
+        IncreaseNumbedChanceForMinions,
+        IncreaseNumbedEffect,
+        InflictAdditionalNumbedStack,
+        IncreaseLightningPenetration,
+        IncreaseMinionLightningPenetration,
+        IncreaseLightningPenetrationOnCriticalStrike,
+        ReduceMaxLifeAndEnergyShieldThresholdForInflictingNumbedOnSupremeBosses,
+        //Convert To Lightning Damage
+        ConvertPhysicalDamageToLightning,
+        ConvertMinionPhysicalDamageToLightningDamage,
+
+        //Erosion
+        IncreaseErosionDamage,
+
+        //Projectile
+        IncreaseProjectileDamage,
+        IncreaseProjectileSpeed,
+        IncreaseMinionProjectileSpeed,
+        IncreaseProjectileCriticalStrikeDamage,
+        IncreaseProjectileDamageWithDistance,
+        IncreaseProjectileDamageAgainstEnemiesInProximity, // Against Nearby Enemies
+        IncreaseParabolicProjectileSplits,
+        IncreaseHorizontalProjectilePenetrations,
+
+        //Increase Addition Damage
+        IncreaseAdditionalMaxDamage,
+        IncreaseAdditionalMinionMaxDamage,
+        IncreaseAdditionBaseDamageForTwoHandedWeapons,
+        IncreaseAdditionalAttackDamageForEachUniqueTypeOfWeaponEquippedWhileDualWielding,
+        IncreaseAdditionalMainHandWeaponDamageWhileHoldingAShield,
+        IncreaseAdditionalDamageAppliedToLife,
+        IncreaseAdditionalAttackDamageAgainstFullLifeEnemies,
+        IncreaseAdditionalBaseDamageForTwoHandedWeapons,
+        IncreaseAdditionFireDamageDealtByTheNextMainSkillEvery1s,
+        IncreaseAdditionAttackDamageIfYouHaveUsedAWarcrySkillInTheLast8s,
+        IncreaseDamageIfDefeatedEnemyRecently,
+        IncreaseAdditionalDamageAfterMobilitySkill,
+
+        //Increase Critical 
+        IncreaseCriticalStrikeDamage,
+        IncreaseMinionCriticalStrikeDamage,
+        IncreaseCriticalStrikeRating,
+        IncreaseMinionCriticalStrikeRating,
+        IncreaseAttackCriticalStrikeRating,
+        IncreaseRangedAttackCriticalStrikeRating, //проверить!
+        IncreaseCriticalStrikeDamageToMeleeAttacks,
+        IncreaseAttackCriticalStrikeRatingWhenHoldingTwoHandedWeapon,
+        IncreaseCriticalStrikeChanceForEachSpellSkillUsedRecently,
+        IncreaseCriticalChance,
+
+        //Increase speed
+        IncreaseAttackSpeed,
+        IncreaseMinionAttackSpeed,
+        IncreaseAttackSpeedAndCastSpeed,
+        IncreaseMinionAttackSpeedAndCastSpeed,
+        IncreaseAttackSpeedWhileDualWielding,
+        IncreaseAttackSpeedWhileHoldingAShield,
+        IncreaseCastSpeed,
+
+        //Increase Additional Attack speed
+        IncreaseAdditionalRangedTwoHandedWeaponBaseAttackSpeed,
+        IncreaseAdditionalAttackSpeedOnCriticalStrikeRecent,
+
+        //Movement
+        IncreaseMovementSpeed,
+        IncreaseMovementSpeedWhenHoldingAShield,
+        ApplyMovementSpeedBonusToCooldownRecoverySpeedForMovementSkills,
+        ApplyMovementSpeedBonusToAttackAndCastSpeedForMovementSkills,
+
+        //Cooldown
+        IncreaseCooldownRecoverySpeedForMobilitySkills,
+
+        //Increase Area 
+        IncreaseSkillArea,
+        IncreaseAreaDamage,
+        IncreaseFireSkillArea,
+        IncreaseWarcrySkillArea,
+        IncreaseSkillAreaIfMainSkillIsNotUsedInTheLast2s,
+
+
+        // Increase Survivability
+        //  Armor
         IncreaseArmor,
+        AddFlatArmor,
+        IncreaseChanceToGainDoubleArmor,
+        IncreaseDefenceGainedFromChestArmor,
+        IncreaseDefence,
+        IncreaseArmorPerStackOfTenacityBlessing,
+        //  Evasion
+        IncreaseEvasion,
+        AddFlatEvasion,
+        IncreaseEvasionWhileMoving,
+        IncreaseEvasionFromChestArmor,
+        IncreaseEvasionPerAgilityBlessingStack,
+        // Avoid 
+        IncreaseChanceToAvoidDamage,
+        IncreaseChanceToAvoidProjectileDamage,
+        IncreaseChanceToAvoidMeleeDamage,
+
+        // Life 
         IncreaseMaxLife,
         IncreaseLifeRegain,
+        IncreaseLifeRegenerationSpeed,
+        IncreaseRegeneratesLifePerSecond,
+        AddFlatLife,
+        RestoreMissingLife,
+        DecreaseAdditionalLifeRegainInterval,
+        RestoreLifeWhenReceivingAgilityBlessing,
+        RestoreMissingLifeWhenSufferSevereInjury,
+        RegainsAdditionalLifeBasedOnMissingEnergyShield,
+        RegainsAdditionalEnergyShieldRegainBasedOnMissingLife,
+
+
+        // Increase Enerygy Shield
+        IncreaseMaxEnergyShield,
+        IncreaseEnergyShieldRegain,
+        RestoreMissingEnergyShield,
+        DecreaseAdditionalEnergyShieldRegainInterval,
+        RestoreMissingEnergyShieldWhenSufferSevereInjury,
+
+        // Increase Mana
         IncreaseMaxMana,
+        RegenerateManaPerSecond,
+        RestoreManaWhenReceivingAgilityBlessing,
 
-        IncreaseFireResistance,
+        // Increase Block
+        IncreaseAttackBlockChance,
+        IncreaseBlockRatioWhenHoldingTwoHandedWeapon,
 
+        // Increase Resistances
+        IncreaseFireResistance, // заменить везде на AddFireResistance
+        AddColdResistance,
+        AddLightningResistance,
 
+        // MainStats
+        // Strength
         IncreaseStrength,
+        AddFlatStrength,
+        // Dexterity
+        IncreaseDexterity,
+        AddFlatDexterity,
+        // Intelligence
 
-        IncreaseMovementSpeed,
+        // Increase Cost
+        IncreaseSkillCost,
 
+        // Decrease
         DecreaseAttackSkillCost,
+        DecreaseSkillCost,
+        DecreasePhysycalDamageTaken,
+        DecreaseAdditionalElementalDamageTaken,
+        DecreaseAttackSpeed,
+        DecreaseAdditionalIgniteDamageTaken,
+        DecreaseIgniteEffectReceived,
+        DecreaseSpellDamage,
+        DecreaseRegainInterval,
 
-        ConvertPhysicalDamageToFireDamage,
+        // Tenacity
+        IncreaseMaxTenacityBlessingStacks,
+        IncreaseTenacityBlessingDuration,
+        IncreaseElementalResistanceWhileTenacityBlessingIsActive,
+        IncreaseChanceToGainOneStacksOfTenacityBlessingWhenTakingDamageInterval1s,
+        IncreaseLifeRegenerationSpeedPerStackOfTenacityBlessingOwned,
+        IncreaseFlatLifeRegenerationPerSecondPerStackOfTenacityBlessingOwned,
 
-        AdditionFireDamageDealtByTheNextMainSkillEvery1s,
-        AdditionAttackDamageIfYouHaveUsedAWarcrySkillInTheLast8s,
+        // Agility Blessing
+        IncreaseChanceToGainAgilityBlessingOnEvasionInterval1s,
+        IncreaseAgilityBlessingDuration,
+        IncreaseAttackSpeedWhileAgilityBlessingIsActive,
+        IncreaseMaxAgilityBlessingStacks,
+        IncreaseChanceToGainAgilityBlessingOnUseSpellSkill,
+
+        // Paralysis
+        IncreaseInflictedParalysisEffectWhenHoldingTwoHandedWeapon,
 
         WarcryCooldownRecoverySpeed,
+        IncreaseMinimumNumberOfEnemiesAffectedByWarcry,
 
         IncreaseAttackSkillLevel,
 
+        // Increase PerStat
+        IncreaseArmorPerStrength,
+        IncreaseElementalResistancePerArmor,
+        IncreaseErosionResistancePerArmor,
+
+        // Knockback
+        IncreaseChanceToCauseKnockbackOnCriticalStrike,
+
+        // Jumps for Projectile/Chain Skills
+        IncreaseJumps,
+
+        // Multistrike
+        IncreaseMultistrikeDamage,
+        IncreaseMinionMultistrikeDamage,
+        IncreaseChanseToMultistrike,
+        IncreaseMinionChanseToMultistrike,
+        IncreaseAdditionalAttackSpeedOnMultistrike,
+        IncreaseAdditionalMinionAttackSpeedOnMultistrike,
+
+        // Injury 
+        IncreaseInjuryBaffer,
+
+        // Affliction
+        IncreaseAfflictionEffect,
+        IncreaseMinionAfflictionEffect,
+        IncreaseFlatAfflictionInflictedPerSecond,
+        IncreaseMinionFlatAfflictionInflictedPerSecond,
+
+        // Other
+        ConsumesLifeOnAttackSkillUse,
+        DamageTakenBypassesEnergyShield,
+
+        // Minios
+        MinionsAreImmuneToFireDamage,
+        MinionImmunityToLightningDamage,
+
+        // Negative Effects
+        IncreaseAdditionalMeleeDamageTaken,
+        IncreaseAdditionalProjectileDamageTaken,
     }
 
 }
--- Models/Stats/StatModifier.cs
+++ Models/Stats/StatModifier.cs
@@ -3,12 +3,29 @@
     public class StatModifier
     {
         public AttributeModifierType AttributeModifier { get; }
-        public double Value { get; }
+        public double? Value { get; }
+        public int? ForEach { get; }
+        public double? MaxValue { get; }
+        public double? Duration { get; }
+        public int? MaxStack { get; }
+        public double? IntervalInSeconds { get; }
 
-        public StatModifier(AttributeModifierType attributeModifier, double value)
+        public StatModifier(
+            AttributeModifierType attributeModifier, 
+            double? value = null, 
+            int? forEach = null, 
+            double? maxValue = null, 
+            double? duration = null, 
+            int? maxStack = null, 
+            double? intervalInSeconds = null)
         {
             AttributeModifier = attributeModifier;
             Value = value;
+            ForEach = forEach;
+            MaxValue = maxValue;
+            Duration = duration;
+            MaxStack = maxStack;
+            IntervalInSeconds = intervalInSeconds;
         }
     }
 }
